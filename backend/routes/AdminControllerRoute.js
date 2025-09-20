ab257981c286335f5ac606becce10f5207aecef9
DIPRO2000 <diprochak@gmail.com>
2025-09-20 13:59:49 +0530

    test

--- backend/routes/AdminControllerRoute.js
+++ backend/routes/AdminControllerRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { NGOApprovalFunction } from "../controllers/AdminController.js";
 
 const router=express.Router();
-
+ 
 router.post("/approval",NGOApprovalFunction);
 
-export default router; 
+export default router;  
